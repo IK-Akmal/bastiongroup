987add934889ddeff69a5872e31ca56abb9d836e
Akmal <isomadinovakmal1997@gmail.com>
2022-07-16 22:04:31 +0500

    added header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import classNames from 'classnames';
 import HeaderTypeProps from './Header.props';
+import styles from './Header.module.scss';
 
 function Header({ className, ...props }: HeaderTypeProps) {
   return (
-    <header className='' {...props}>
+    <header className={classNames(className, styles.header)} {...props}>
       Header
     </header>
   );

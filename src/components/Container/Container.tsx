a23aafbce99bccf1879a978c9e000360c5ee1ac2
Akmal <isomadinovakmal1997@gmail.com>
2022-07-17 00:07:05 +0500

    added container

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from 'react';
+import styles from './Container.module.scss';
+
+function Container({ children }: PropsWithChildren) {
+  return (
+    <div className={styles.container}>{children}</div>
+  );
+}
+
+export default Container;

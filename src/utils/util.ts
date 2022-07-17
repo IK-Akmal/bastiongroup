ddde9401f0a49f091786bcfc7842c09f47a0bccf
Akmal <isomadinovakmal1997@gmail.com>
2022-07-18 02:12:45 +0500

    added utils

---
+++ src/utils/util.ts
@@ -0,0 +1,9 @@
+export default function toArray<T>(candidate?: T | T[] | false): T[] {
+  if (candidate === undefined || candidate === false) return [];
+
+  return Array.isArray(candidate) ? candidate : [candidate];
+}
+
+export function keyGen(): string {
+  return Math.random().toString();
+}

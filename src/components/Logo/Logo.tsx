a6b5b05ad2ab51961c1d5de3881ea21af56a0c3e
Akmal <isomadinovakmal1997@gmail.com>
2022-07-17 14:49:39 +0500

    added logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,21 @@
+import styles from './Logo.module.scss';
+import LogoSmall from './img/Logo-1x.png';
+import LogoMedium from './img/Logo-2x.png';
+
+function Logo() {
+  return (
+    <div className={styles.logo}>
+      <img
+        className={styles.img}
+        src={LogoSmall}
+        alt="logo"
+        srcSet={`${LogoSmall} 1x, ${LogoMedium} 2x`}
+      />
+      <h1 className={styles.text}>
+        Производитель металлических изделий №1
+      </h1>
+    </div>
+  );
+}
+
+export default Logo;

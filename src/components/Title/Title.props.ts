c826fcd0cb482eee8965906331e138e03c1f236d
Akmal <isomadinovakmal1997@gmail.com>
2022-07-18 15:04:13 +0500

    added title component

---
+++ src/components/Title/Title.props.ts
@@ -0,0 +1,6 @@
+interface TitleTypeProps {
+  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  className?: string;
+}
+
+export default TitleTypeProps;
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames';
+import { PropsWithChildren } from 'react';
+import styles from './Title.module.scss';
+import TitleTypeProps from './Title.props';
+
+function Title({ variant = 'h1', className, children }: PropsWithChildren<TitleTypeProps>) {
+  const Tag = `${variant}` as keyof JSX.IntrinsicElements;
+  return (
+    <Tag className={classNames(styles.title, className)}>{children}</Tag>
+  );
+}
+
+export default Title;
